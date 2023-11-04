8fe88298b2c7cd332998a7b2c1c74877f0e54073
RumiYo <yokoyamarumi@gmail.com>
2023-11-03 22:47:24 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  function bioExist (bio) {
+    console.log(bio)
+    if (bio === undefined || bio.length === 0 ){
+      console.log('No')
+      return null;
+   }else{    
+    console.log("yes")
+      return (
+        <p>{bio}</p>
+      )
+   }
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioExist(props.bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
+
+
 
 export default About;
